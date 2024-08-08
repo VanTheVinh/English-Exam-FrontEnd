12ec2d502da64a92964435b33ab99e864eba529e
anhquoc0912 <leanhquoc1611@gmail.com>
2024-08-08 10:47:42 +0700

    giao diện quên mật khẩu

--- src/pages/forgotPassword/index.js
+++ src/pages/forgotPassword/index.js
@@ -3,8 +3,25 @@ import styles from './ForgotPassword.module.scss';
 
 const ForgotPassword = () => {
     return (
-        <div className={styles.forgot_password}>
-            <h1>ForgotPassword</h1>
+        <div>
+            <div className={styles.header}>
+                <img src='logo192.png'></img>
+                <h5>Name</h5>
+            </div>
+            <div className={styles.content}>
+                <div className={styles.reset_password_form}>
+                    <h4>Reset password</h4>
+                    <div className={styles.email_label} >
+                        <label for='email'>Email Address</label>
+                    </div>
+                    <input type="email" id="email" name="email" placeholder="john.doe@example.com"></input>
+                    <div>
+                        <button>
+                            SEND
+                        </button>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
