63138695f160d6310371a8b6f3e6e033e5044228
Bin-Ne <vinhvlog2108@gmail.com>
2024-08-23 10:30:33 +0700

    Update Dashboard teacher

--- src/pages/teacher/dashboard/index.js
+++ src/pages/teacher/dashboard/index.js
@@ -1,179 +1,60 @@
-import React, { useState } from 'react';
 import styles from './Dashboard.module.scss';
-import logo from '~/pages/teacher/dashboard/logo512.png';
+import Header from '~/components/header';
+
+import React, { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faTachometerAlt,
+  faUser,
+  faChalkboardTeacher,
+  faUsers,
+  faFileAlt,
+  faQuestionCircle,
+  faChartLine,
+  faSignOutAlt,
+} from '@fortawesome/free-solid-svg-icons';
+
+import ManageClasses from '../manageClasses';
+import ManageStudents from '../manageStudents';
+import ManageExams from '../manageExams';
+import ManageQuestions from '../manageQuestions';
+import PublishResults from '../publishResults';
 
 const Dashboard = () => {
   const [page, setPage] = useState(0);
+
+  const menuItems = [
+    { name: 'Dashboard', icon: faTachometerAlt },
+    { name: 'Profile', icon: faUser},
+    { name: 'Classes', icon: faChalkboardTeacher, component: <ManageClasses />},
+    { name: 'Students', icon: faUsers, component: <ManageStudents /> },
+    { name: 'Tests', icon: faFileAlt, component: <ManageExams /> },
+    { name: 'Question', icon: faQuestionCircle, component: <ManageQuestions /> },
+    { name: 'Scores', icon: faChartLine, component: <PublishResults /> },
+    { name: 'Sign Out', icon: faSignOutAlt },
+  ];
+
   return (
     <div className={styles.dashboard}>
-      <div className={styles.Khung}>
-        <div className={styles.footer}>
-          <img className={styles.avatar} src={logo} alt="" />
-          <img
-            className={styles.arrow}
-            width="20"
-            height="20"
-            src="https://img.icons8.com/ultraviolet/40/expand-arrow--v1.png"
-            alt="expand-arrow--v1"
-          />
-        </div>
+      <Header />
+      <div className={styles.container}>
         <div className={styles.main}>
           <div className={styles.menu}>
             <ul>
-              {page == 1 ? (
+              {menuItems.map((item, index) => (
                 <li
-                  onClick={() => {
-                    setPage((e) => (e = 0));
-                  }}
-                  className={styles.active}
+                  key={index}
+                  className={page === index ? styles.active : ''}
+                  onClick={() => setPage(index)}
                 >
-                  Dashboard
+                  <FontAwesomeIcon icon={item.icon} className={styles.icon} />
+                  {item.name}
                 </li>
-              ) : (
-                <li
-                  onClick={() => {
-                    setPage((e) => (e = 1));
-                  }}
-                >
-                  Dashboard
-                </li>
-              )}
-              {page == 2 ? (
-                <li
-                  className={styles.active}
-                  onClick={() => {
-                    setPage((e) => (e = 0));
-                  }}
-                >
-                  Profile
-                </li>
-              ) : (
-                <li
-                  onClick={() => {
-                    setPage((e) => (e = 2));
-                  }}
-                >
-                  Profile
-                </li>
-              )}
-              {page == 3 ? (
-                <li
-                  className={styles.active}
-                  onClick={() => {
-                    setPage((e) => (e = 0));
-                  }}
-                >
-                  Classes
-                </li>
-              ) : (
-                <li
-                  onClick={() => {
-                    setPage((e) => (e = 3));
-                  }}
-                >
-                  Classes
-                </li>
-              )}
-              {page == 4 ? (
-                <li
-                  className={styles.active}
-                  onClick={() => {
-                    setPage((e) => (e = 0));
-                  }}
-                >
-                  Students
-                </li>
-              ) : (
-                <li
-                  onClick={() => {
-                    setPage((e) => (e = 4));
-                  }}
-                >
-                  Students
-                </li>
-              )}
-              {page == 5 ? (
-                <li
-                  className={styles.active}
-                  onClick={() => {
-                    setPage((e) => (e = 0));
-                  }}
-                >
-                  Tests
-                </li>
-              ) : (
-                <li
-                  onClick={() => {
-                    setPage((e) => (e = 5));
-                  }}
-                >
-                  Tests
-                </li>
-              )}
-              {page == 6 ? (
-                <li
-                  className={styles.active}
-                  onClick={() => {
-                    setPage((e) => (e = 0));
-                  }}
-                >
-                  Question
-                </li> 
-              ) : (
-                <li
-                  onClick={() => {
-                    setPage((e) => (e = 6));
-                  }}
-                >
-                  Question
-                </li>
-              )}
-              {page == 7 ? (
-                <li
-                  className={styles.active}
-                  onClick={() => {
-                    setPage((e) => (e = 0));
-                  }}
-                >
-                  Scores
-                </li>
-              ) : (
-                <li
-                  onClick={() => {
-                    setPage((e) => (e = 7));
-                  }}
-                >
-                  Scores
-                </li>
-              )}
-              {page == 8 ? (
-                <li
-                  className={styles.active}
-                  onClick={() => {
-                    setPage((e) => (e = 0));
-                  }}
-                >
-                  Sign Out
-                </li>
-              ) : (
-                <li
-                  onClick={() => {
-                    setPage((e) => (e = 8));
-                  }}
-                >
-                  Sign Out
-                </li>
-              )}
+              ))}
             </ul>
           </div>
-          <div className={styles.mainshow}>
-            {page == 1 ? <p>Dashboard</p> : null}
-            {page == 2 ? <p>Profile</p> : null}
-            {page == 3 ? <p>Classes</p> : null}
-            {page == 4 ? <p>Students</p> : null}
-            {page == 5 ? <p>Tests</p> : null}
-            {page == 6 ? <p>Question</p> : null}
-            {page == 7 ? <p>Scores</p> : null}
+          <div className={styles.mainContent}>
+            {menuItems[page].component}
           </div>
         </div>
       </div>
